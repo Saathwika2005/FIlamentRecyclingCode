bb14648985135d4c3050388b7398260497f497e9
Vijesh Shetty <84760221+VijeshVS@users.noreply.github.com>
2024-05-25 13:52:24 +0530

    Code refactoring

--- Filament_Recycling.ino
+++ Filament_Recycling.ino
@@ -1,13 +1,15 @@
 #include <math.h>
-const int thermistor_output = A1;
+
+#define thermistor_output A1
+
+int thermistor_adc_val;
+double output_voltage, thermistor_resistance, therm_res_ln, temperature; 
 
 void setup() {
-  Serial.begin(9600);	/* Define baud rate for serial communication */
+  Serial.begin(9600);
 }
 
-void loop() {
-  int thermistor_adc_val;
-  double output_voltage, thermistor_resistance, therm_res_ln, temperature; 
+int getThermistorTemperature(){
   thermistor_adc_val = analogRead(thermistor_output);
   output_voltage = ( (thermistor_adc_val * 5.0) / 1023.0 );
   thermistor_resistance = ( (47*output_voltage)/(5-output_voltage) ); /* Resistance in kilo ohms */
@@ -15,9 +17,16 @@ void loop() {
   therm_res_ln = log(thermistor_resistance);
   /*  Steinhart-Hart Thermistor Equation: */
   /*  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)   */
-
   temperature = ( 1 / ( 0.00053085725+ ( 0.00023960398 * therm_res_ln ) +( 0.0000000423434340345 * therm_res_ln * therm_res_ln * therm_res_ln ) ) ); /* Temperature in Kelvin */
   temperature = temperature - 273.15; /* Temperature in degree Celsius */
+
+  return temperature;
+}
+
+void loop() {
+
+  int temperature = getThermistorTemperature();
+
   Serial.print("Temperature in degree Celsius = ");
   Serial.print(temperature);
   Serial.print("\t\t");
