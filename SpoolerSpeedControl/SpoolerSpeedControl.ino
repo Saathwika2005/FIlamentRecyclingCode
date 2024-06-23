963d17d78db1e246ba8b49604e01c43b403a1fe3
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-06-23 21:53:13 +0530

    spooler speed control done

--- SpoolerSpeedControl/SpoolerSpeedControl.ino
+++ SpoolerSpeedControl/SpoolerSpeedControl.ino
@@ -1,5 +1,18 @@
-#include <PID_v1_bc.h>
 #include <Encoder.h>
+
+
+// PID control parameters
+float Kp = 100;
+float Ki = 5;
+float Kd = 0;
+
+float desiredSpeed = 0.2; // Desired speed in rps
+
+float previousError = 0;
+
+// integral value assuming that minimum speed of the spooler motar is 0.15 rps
+float integral = 19.6;
+unsigned long lastTime = 0;
 
 // Encoder setup
 Encoder myEnc(13, 12);
@@ -13,14 +26,7 @@ double prevTime;
 int In1 = 7;
 int In2 = 8;
 int ENA = 5;
-const int MIN_PWM = 100; // Minimum PWM value to start motor rotation
 
-// PID setup
-double Setpoint = 0.2; // Desired RPS
-double Input;
-double Output;
-double Kp = 200.0, Ki = 25.0, Kd = 10.0; // Adjusted PID coefficients
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
   Serial.begin(9600);
@@ -31,37 +37,53 @@ void setup() {
   digitalWrite(In1,HIGH);
   digitalWrite(In2,LOW);
 
+  analogWrite(ENA, 90);
+
   time = millis()/1000.0;
   prevTime = 0;
 
-  // Initialize PID controller
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(MIN_PWM, 255); // Output limits corresponding to PWM range, with a minimum threshold
 }
+
+void PidControl(double rps){
+  unsigned long currentTime = millis();
+  float deltaTime = (currentTime - lastTime) / 1000.0;
+  lastTime = currentTime;
+
+  float error = desiredSpeed - rps;
+
+  float P = Kp * error;
+  integral += error * deltaTime;
+  float I = Ki * integral;
+  float D = Kd * (error - previousError) / deltaTime;
+
+  float pidOutput = P + I + D;
+
+  Serial.print("PidOutput: ");
+  Serial.println(pidOutput);
+
+  Serial.print("Integral: ");
+  Serial.println(integral);
+
+  analogWrite(ENA, pidOutput);
+
+  previousError = error; // Update previous error for next iteration
+}
+
+
 
 void loop() {
   time = millis()/1000.0;
-  rotations = myEnc.read()/60.0;
+  rotations = myEnc.read()/60.0*-1;
 
   if((time-prevTime) > 0.5) {
     double rps = (rotations-prevRotations)/(time-prevTime);
     Serial.print("RPS: ");
     Serial.println(rps);
 
-    // PID control
-    Input = abs(rps);
-    myPID.Compute();
-
-    // Ensure the output is at least the minimum value to start the motor
-    if (Output < MIN_PWM) {
-      Output = MIN_PWM;
-    }
-    
-    Serial.print("Output: ");
-    Serial.println(Output);
-    analogWrite(ENA, Output);
+    PidControl(rps);
 
     prevTime = time;
     prevRotations = rotations;
   }
+
 }
